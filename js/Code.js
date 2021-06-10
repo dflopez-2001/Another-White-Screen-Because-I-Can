98df02d97158298966dcd41d6998c8a7836f948e
dflopez-2001 <83239928+dflopez-2001@users.noreply.github.com>
2021-06-10 11:09:08 -0700

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("T I O C F U N N", 800,50)
+fill("lightblue")
+text("Hint: Idk", 800,70) 
+
+fill("white")
+textSize(15)
+text("B E S A A D T", 100,200)
+fill("lightblue")
+text("Hint: Theres no answer theres no way to win", 100,210)
+}
